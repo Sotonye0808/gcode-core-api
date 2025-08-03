bb98ae59dfb0b5d217fbffe682db471c55cc6bcf
Sotonye0808 <sotydagz@gmail.com>
2025-08-03 20:25:49 +0100

    minor addition

--- gcode_core/wsgi.py
+++ gcode_core/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from dotenv #import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
+#project_folder = os.path.expanduser('/gcode_core/')
+#load_dotenv(os.path.join(project_folder, '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gcode_core.settings')
 
 application = get_wsgi_application()
